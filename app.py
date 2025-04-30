9daa6a8b164e1c2d0b3bf0bed646dbb26fc29266
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-04-30 13:56:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -520,5 +520,5 @@ def check_model_status():
 # Inicializa a aplicação
 inicializa_modelo_padrao()  # Chama a inicialização sempre que o app é carregado
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#if __name__ == '__main__':
+    #app.run(debug=True, host='0.0.0.0', port=5000)
