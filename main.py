7d7537489e4cf1c82ed041e59eb8e964d77904de
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-04-30 13:55:06 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from app import app as temis_app
+
+application = DispatcherMiddleware(Flask('dummy'), {
+    '/temis': temis_app
+})
