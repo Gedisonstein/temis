d1cde823722fb3a2b879f90657ba0bf8ee8107ec
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-05-01 11:28:55 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from app import app as temis_app
-
-application = DispatcherMiddleware(Flask('dummy'), {
-    '/temis': temis_app
-})
