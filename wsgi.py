8152910f69ec38b0ac40a36d6146df687058e245
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-05-01 11:09:19 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from app import app
 
 if __name__ == "__main__":
+    # Use o werkzeug run_simple para executar a aplicação middleware
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 5000, application, use_reloader=True)
     app.run()
