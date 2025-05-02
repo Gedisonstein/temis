088a52aca94ffce0047e6c2d1c1d2869c4209795
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-05-02 14:47:10 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,10 @@
+from app import app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from temis import application  # ou o nome do seu arquivo principal sem .py
 
 application = DispatcherMiddleware(None, {
-    '/temis': application
+    '/temis': app
 })
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+    run_simple('0.0.0.0', 8000, application)
