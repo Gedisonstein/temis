f2ea85ae9c504804b304f05d3bc11477c60bb539
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-05-02 15:13:36 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from app import app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from app import app as temis_app
 
-application = DispatcherMiddleware(Flask('dummy'), {
-    '/temis': temis_app
+application = DispatcherMiddleware(None, {
+    '/temis': app
 })
+
+if __name__ == "__main__":
+    from werkzeug.serving import run_simple
+    run_simple('0.0.0.0', 8000, application)
