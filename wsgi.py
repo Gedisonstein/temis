bc1144b39bdbf1779be9808b440f2dd796b2fc75
Gedisonstein <59055098+Gedisonstein@users.noreply.github.com>
2025-05-02 14:17:49 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,13 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from werkzeug.wrappers import Response
+from app import app  # importa o Flask app normal
+
+application = DispatcherMiddleware(
+    Response('Not Found', status=404),
+    {
+        '/temis': app
+    }
+)
 from app import app
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
